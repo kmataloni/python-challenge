45ddebe81dca5a17a74c0375e01f7f4b80e8276d
Kyle <kmatalon12@gmail.com>
2021-05-22 09:47:50 -0500

    resubmit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,25 @@
 import os
 import csv
 
-csvoutput = os.path.join(".","analysis", "budget_analysis.txt")
-csvpath = os.path.join(".","Resources", "budget_data.csv" )
+csvoutput = os.path.join(".", "analysis", "budget_analysis.txt")
+csvpath = os.path.join(".", "Resources", "budget_data.csv")
 
-total_months = []
+# initializing variables
+total_months = 1
 
-net_change = [] 
+total_profit = 0
 
+change = 0
+
+net_change = 0
+
+average_change = 0
+
+greatest_increase = ["", 0]
+
+greatest_decrease = ["", 9999999999999]
+
+#reading csv file
 with open(csvpath) as csvfile:
     print(csvpath)
     csvreader = csv.reader(csvfile)
@@ -15,40 +27,67 @@ with open(csvpath) as csvfile:
     # Read the header row
     header = next(csvreader)
     print(header)
-
-    for row in csvreader:
-        
-        net_change.append(int(row[1]))
-        total_months = len(total_months)
-
-        print(total_months)
-#     total_profit = 0
     
-#     #Count number of months
-#     for row in csvreader:
-#         total_months += 1
+    first_row = next(csvreader)
+    print(first_row)
 
-#         total_profit = total_profit + int(row[1])   
+    # Add January into total profit
+    total_profit += int(first_row[1])
+    # Define previous month
+    previous_month = (first_row[1])
+
+    # Count number of months
+    for row in csvreader:
+        total_months += 1
         
-#         average_change = 
+        total_profit = total_profit + int(row[1])   
+        
 
-# print(total_months)
+        # Difference between current month and previous month
 
-# print(total_profit)
+        change = int(row[1]) - int(previous_month)
+        
+        # increment previous month January becomes February
+        previous_month = row[1]
+        
+        # print(change)
 
-# Financial_Analysis = ('Total Months: ' + str(total_months))
- 
-# # print(Financial_Analysis)   
+        net_change +=change
+
+        if  change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = change
+        
+        if  change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = change
+
+
+    average_change = net_change / (total_months - 1)
+   
+
+# # print(total_months)
+
+# # print(total_profit)
+
+Financial_Analysis = f'''
+    Financial Analysis
+    -----------------------
+    Total Months: {total_months}
+    Total: ${total_profit}
+    Average Change: {round(average_change,2)}
+    Greatest Increase in Profits: {greatest_increase[0]} ${greatest_increase[1]}
+    Greatest Decrease in Profits: {greatest_decrease[0]} ${greatest_decrease[1]}
+    '''
+
+
+print(Financial_Analysis)   
 
 
 
 
-
-#  #Export output
-# with open(csvoutput, 'w') as text_file:
-#     text_file.write(Financial_Analysis)
-
-
-
+ #Export output
+with open(csvoutput, 'w') as txt_file:
+    txt_file.write(str(Financial_Analysis))
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,27 +3,57 @@ import csv
 
 
 csvpath = os.path.join(".", "Resources", "election_data.csv")
-csvoutput = os.path.join(".", "Resources", "election_analysis.txt")
+csvoutput = os.path.join(".", "analysis", "election_analysis.txt")
 
-total_votes = 0
+votes = 0
+vote_count = []
 candidate_list = []
-vote_percent = /total_votes
 
-print(csvpath)
+
+
 with open(csvpath) as csvfile:
     print(csvpath)
     csvreader = csv.reader(csvfile)
-
     header = next(csvreader)
-    print(header)
+    
+    for row in csvreader:
+        votes = votes +1
+        candidate = row[2]
 
-#     for row in csvreader:
-#         total_votes += 1
-#         candidate_list = str([2])
+        if candidate in candidate_list:
+            candidate_index = candidate_list.index(candidate)
+            vote_count[candidate_index] = vote_count[candidate_index] + 1
+        else:
+            candidate_list.append(candidate)
+            vote_count.append(1)
 
-#         print(total_votes)
+percentages = []
+vote_winner = vote_count[0]
+vote_winner_index = 0
 
-#         print(candidate_list)
+for count in range(len(candidate_list)):
+    vote_percentage = vote_count[count]/votes*100
+    percentages.append(vote_percentage)
+    if vote_count[count] > vote_winner:
+        print(vote_winner)
 
+winner = candidate_list[vote_winner_index]
+percentages = [round (i,2) for i in percentages]
 
+print()
 
+poll_analysis = f'''
+Election Results
+---------------------
+Total Votes: {votes}
+---------------------
+{candidate_list[count]}: {percentages[count]}% {vote_count[count]}
+---------------------
+Winner: {winner}
+---------------------
+'''
+
+print(poll_analysis)
+
+with open(csvoutput, 'w') as txt_file:
+    txt_file.write(str(poll_analysis))
